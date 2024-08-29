7beffdbb9e59fd11a447d0cd358cad30c3e678c4
weslley vicente <weslleyvicente373@gmail.com>
2024-08-29 11:50:16 -0300

    Add files via upload

---
+++ 04-nome-idade.py
@@ -0,0 +1,12 @@
+nome = ("Weslley Vicente")
+idade = 16
+altura = 1.75
+print ("Nome: ",nome,"\nIdade: ",idade, "\n Altura: ", altura)
+print(f"nome: {nome} idade: {idade} altura: {altura}")
+
+nome = input("Digite o seu nome: ")
+idade = int(input("Digite a sua idade: "))
+altura = float(input("Digite a sua altura: "))
+print(f"nome: {nome} idade: {idade} altura: {altura}")
+
+
