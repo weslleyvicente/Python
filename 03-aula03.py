7d2329b6a5a33adc26257cd528158c8ef261c489
weslley vicente <weslleyvicente373@gmail.com>
2024-08-29 11:50:01 -0300

    Add files via upload

---
+++ 03-aula03.py
@@ -0,0 +1,12 @@
+teste = "Weslley"
+print(teste) #variável str (string)
+print(type(teste))
+teste = 10
+print(teste) #variável int (inteiro)
+print(type(teste))
+teste = 1.71
+print(teste) #variável float 
+print(type(teste))
+teste = True
+print(teste) #variável bool (booleano)
+print(type(teste))
