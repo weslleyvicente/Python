30c9954d38460160a5570ab13a7307c6218818dd
weslley vicente <weslleyvicente373@gmail.com>
2024-09-10 10:55:34 -0300

    Add files via upload

---
+++ 05-tipos-dados.py
@@ -0,0 +1,13 @@
+# tipos de dados em python 
+# Inteiros
+inteiro = 10
+print(inteiro)
+# floats
+float = 10.5
+print(float)
+# strings
+string = "Olá, mundo!"
+print(string)
+# boolean
+booleano = True
+print(booleano)
