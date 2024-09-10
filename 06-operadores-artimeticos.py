dec9703317fb2b25dfd7b691b2e35ac89613cdd7
weslley vicente <weslleyvicente373@gmail.com>
2024-09-10 11:03:36 -0300

    Add files via upload

---
+++ 06-operadores-artimeticos.py
@@ -0,0 +1,24 @@
+#Operadores aritméticos
+#soma
+soma = 10 + 5
+print(soma)
+#subtracao
+subtracao = 10 - 5
+print(subtracao)
+#multiplicacao
+multiplicacao = 10 * 5
+print(multiplicacao)
+#divisao
+divisao = 10 / 5
+print(divisao)
+#divisao inteira
+divisaointeira = 10 // 5
+print(divisaointeira)
+#potenciacao
+exponenciacao = 10 ** 5
+print(exponenciacao)
+#resto
+resto = 10 % 5
+print(resto)
+
+
